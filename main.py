6f9731f7d0d619041ccdaa1aebe633fe1d73d793
David Dean <runfromnuke@gmail.com>
2025-05-18 16:36:23 -0500

    docs: added thorough comments to script
    I imagine that this might be an unusual use of the `docs` commit type,
    but it seems apropos enough.  Should probably start writing comments
    with my code to begin with so the one commit _with_ the functional code
    also explains it to whoever is reading/reviewing the code.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def get_book_text(filepath):
 
 
 def check_args():
+    # Only continue if one argument is passed after the `*.py` script.
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
@@ -20,14 +21,19 @@ def check_args():
 
 
 def main():
-    # book_file = "./books/frankenstein.txt"
+    # Check for two arguments and read in the second one.
     book_file = check_args()
+    # Copy contents of file to malliable string value.
     book_contents = get_book_text(book_file)
+    # Split into individual words and return word count.
     doc_words = count_words(book_contents)
+    # Split into individual letters and return usage count of unique character
+    # values.
     doc_letters = count_chars(book_contents)
 
-    # Manipulate compiled data
+    # Return list of "readable" characters and their respective count.
     sorted_letters = list_readable(doc_letters)
+    # Sort list of "readable" characters.
     sorted_letters.sort(reverse=True, key=sort_by_count)
 
     # Print report to user
--- stats.py
+++ stats.py
@@ -1,38 +1,61 @@
 def count_words(contents):
+    # Convert full string from file into list of individual words.
     words = contents.split()
+    # Initiate variable for counting the number of words.
     num_words = 0
 
+    # Iterate over the full list and keep a running count.
     for word in words:
         num_words += 1
 
+    # Probably should have done this more simply as:
+    # `return len(words)`
+    # ...but hindsight's 20/20.
     return num_words
 
 
 def count_chars(contents):
+    # Normalize all of the individual characters to their lowercase equivalent.
     standardized = contents.lower()
+    # Initiate empty dictionary
     character_count = {}
 
+    # Iterate over every character...
     for key in standardized:
+        # ...if that character already exists in the dictionary...
         if key in character_count:
+            # ...add one to the count.
             character_count[key] += 1
         else:
+            # If the character is showing for the first time, count the first
+            # instance.
             character_count[key] = 1
 
     return character_count
 
 
+# Literally handed to you in the "Hint" of the "Assignment".
 def sort_by_count(dict):
     return dict["num"]
 
 
 def list_readable(character_count):
+    # Initialize an empty list.
     list_char_count = []
 
+    # For each key in the passed dictionary...
     for key in character_count:
+        # ...initialize a new empty dictionary...
         letter = {}
 
+        # ...and if that value is a "readable" unicode character...
         if str(key).isalpha():
+            # ...add it and the respective count to a more "searchable"
+            # dictionary structure...
             letter = {"char": key, "num": character_count[key]}
+            # ...and add that dictionary to the list.
             list_char_count.append(letter)
 
+    # Once the list has been populated with ONLY readable characters, then
+    # pass it back.
     return list_char_count
